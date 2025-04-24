788d0369424f5d95e169e7610af47dc516c336e7
JiSeon <jsseong0416@gmail.com>
2025-04-24 18:01:31 +0900

    내 파트 오늘 작업한거 정리

--- FinalProject/src/main/java/com/example/FinalProject/dto/SalesDto.java
+++ FinalProject/src/main/java/com/example/FinalProject/dto/SalesDto.java
@@ -1,41 +1,44 @@
 package com.example.FinalProject.dto;
 
+import org.apache.ibatis.type.Alias;
+
 import lombok.Data;
 
+@Alias("SalesDto")
 @Data
 public class SalesDto {
-	int adminsaleid;//지점 adminsale 고유번호
-	int ceosaleid;//본사 ceosale 고유번호
-	String storename;//매장번호 = tb_user.storename
-	String salename;//매출 항목
-	String credate;//매출 날짜
-	String editdate;//매출 수정 날짜
+	int adminsale_id;//지점 adminsale 고유번호
+	int ceosale_id;//본사 ceosale 고유번호
+	String store_name;//매장번호 = tb_user.storename
+	String sale_name;//매출 항목
+	String cre_date;//매출 날짜
+	String edit_date;//매출 수정 날짜
 	int price;//항목 금액
-	String acode;//구분코드 : 수입/지출
-	String bcode;//상세구분코드 : 수업료수입, 기타수입,강사월급, 발주비용, 기타지출
+	String a_code;//구분코드 : 수입/지출
+	String b_code;//상세구분코드 : 수업료수입, 기타수입,강사월급, 발주비용, 기타지출
 	String auto;//default: yes(발주나 수업에서 처리됨), no: 담당자가 직접 입력
 
 	//쿼리문 관련
-	int totalprofit;//총수입
-	int totalcost;//총지출
-	int totalprice;//총금액
+	int total_profit;//총수입
+	int total_cost;//총지출
+	int total_price;//총금액
 	String sdate;//문자열로 변환된 date 타입의 data
 	String smonth;//상동  
 	String syear;//상동
 	//수업 관련 : tb_class, tb_student_class
-	String clsstatus;//수업 상태 : 진행중
-	int classid;//수업 고유 번호
-	int clsprice;//수업료
-	int studentcount;//학생 수
+	String cls_status;//수업 상태 : 진행중
+	int class_id;//수업 고유 번호
+	int cls_price;//수업료
+	int student_count;//학생 수
 	
 	//발주 관련 :tb_product, tb_order, tb_order_detail
-	int orderid;//발주고유번호
-	int orderstatus;//발주상태
-	int orderdetailid;//발주 품목의 고유번호
-	int productid;//tb_product.productid
+	int order_id;//발주고유번호
+	int order_status;//발주상태
+	int order_detail_id;//발주 품목의 고유번호
+	int product_id;//tb_product.productid
 	int quantity;//수량
-	int productprice;//개당 가격
-	int productname;//품목 이름
+	int product_price;//개당 가격
+	int product_name;//품목 이름
 	
 	
 
--- FinalProject/src/main/java/com/example/FinalProject/mapper/SalesMapper.java
+++ FinalProject/src/main/java/com/example/FinalProject/mapper/SalesMapper.java
@@ -1,6 +1,7 @@
 package com.example.FinalProject.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -11,18 +12,22 @@ import com.example.FinalProject.dto.SalesDto;
 public interface SalesMapper {
 	//Admin 측
 	//지점번호와 bcode를 넣어서 수입/지출 리스트를 가져오기
-	List<SalesDto> getAdminSalesList(@Param("storenum") int storenum, @Param("bcodes") List<String> bcodes);
+	List<SalesDto> getAdminSalesList(Map<String, Object> map);//@Param("store_name") int store_name, @Param("b_codes") List<String> b_codes
 	SalesDto getAdminSalebyId(int adminsaleid);
 	int insertAdminSales(SalesDto dto);
 	int editAdminSales(SalesDto dto);
 	int deleteAdminSales(int adminsaleid);//
 	//발주가 "승인"일 때 발주 비용 추가하는 메소드와 관련 메소드(발주에서 사용)
 	int insertOrderApprovedCost(SalesDto dto);
-	int countByClassId(@Param("classid") int classid);
-	int getPriceByClassId(@Param("classid") int classid);
-	String getNameByClassId(@Param("classid") int classid);
+	SalesDto getApprovedOrderId(String status);
+	List<SalesDto> getOrderDetailByOrderId(@Param("order_id") int order_id);
+	SalesDto getNameByProductId(@Param("order_id") int order_id);
 	//수업이 '진행중'일 때 수업료 수입을 추가하는 메소드와 관련 메소드(수업에서 사용)
 	int insertClsIngProfit(SalesDto dto);
+	SalesDto getProfitByClassId(int class_id);
+//	int countByClassId(@Param("classid") int classid);
+//	SalesDto getPriceByClassId(@Param("classid") int classid);
+	SalesDto getNameByClassId(@Param("class_id") int class_id);
 	//연도별 수입 데이터 불러오기
 	List<SalesDto> getAdminProfitStatbyYear();
 	//연도별 지출 데이터 불러오기
