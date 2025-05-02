0b39968333b670a5beaa8eeb5376d85c0b120ea1
JISEON <jsseong0416@gmail.com>
2025-05-02 11:05:34 +0900

    노트북 로컬 커밋

--- FinalProject/src/main/java/com/example/FinalProject/dto/AdminSalesDto.java
+++ FinalProject/src/main/java/com/example/FinalProject/dto/AdminSalesDto.java
@@ -15,16 +15,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class AdminSalesDto {
-	private int adminsale_id;//지점 adminsale 고유번호
-	private String store_name;//매장번호 = tb_user.storename
-	private String sale_name;//매출 항목
-	private String cre_date;//매출 날짜
-	private String edit_date;//매출 수정 날짜
-	private int price;//항목 금액
-	private String a_code;//구분코드 : 수입/지출
-	private String b_code;//상세구분코드 : 수업료수입, 기타수입,강사월급, 발주비용, 기타지출
-	private List<String> Search;//검색조건
-	private String auto;//default: yes(발주나 수업에서 처리됨), no: 담당자가 직접 입력
-	private int startRowNum;
-	private int endRowNum;
+private int adminSaleId;//지점 adminsale 고유번호
+private Integer userId;//원장 ID(매장번호) = tb_user.storename
+private String saleName;//매출 항목
+private String creDate;//매출 날짜
+private String editDate;//매출 수정 날짜
+private int price;//항목 금액
+private String cdAcode;//구분코드 : 수입/지출
+private String cdBcode;//상세구분코드 : 수업료수입, 기타수입,강사월급, 발주비용, 기타지출
+private List checkedItems;//검색조건
+private String auto;//default: yes(발주나 수업에서 처리됨), no: 담당자가 직접 입력
+private int startRowNum;
+private int endRowNum;
+private int pageNum;
+private List list;
+private int startPageNum;
+private int endPageNum;
+private int totalPageCount;
+private int totalRow;
+
 }
